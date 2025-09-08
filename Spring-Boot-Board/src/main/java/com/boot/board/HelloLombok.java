449974137e9432e2dea278e80f809bfb02e39abd
User <User@DESKTOP-USTO23V>
2025-09-08 18:24:41 +0900

    250908

---
+++ Spring-Boot-Board/src/main/java/com/boot/board/HelloLombok.java
@@ -0,0 +1,41 @@
+package com.boot.board;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class HelloLombok {
+
+	private final String name;
+	private final int age;
+	
+	/*
+	public static void main (String[] args) {
+		
+//		HelloLombok hk = new HelloLombok();
+//		
+//		hk.setName("±èÀ±È£");
+//		hk.setAge(31);
+		
+		HelloLombok hk = new HelloLombok("¹è¼öÁö",30);
+		
+		System.out.println(hk.getName());
+		System.out.println(hk.getAge());
+		
+	}
+	*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
